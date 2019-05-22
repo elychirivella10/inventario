a3e1265c79e982ad5442223f07b74c4a770ce0f3
Craig2412 <aularalexander55@gmail.com>
2019-05-22 13:49:55 -0400

    sn

---
+++ borrarr.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+	<center><h1>
+	
+
+
+
+
+
+		<?php 
+
+include 'conexion.php';
+
+$id = $_REQUEST['id'];
+
+
+$el = $conec -> query("DELETE FROM inventarioo WHERE id = '$id'");
+
+if ($el) {
+	echo "El registro ha sido eliminado";
+	echo "<script>
+	location.href='inventario.php';
+	</script>";
+}else
+echo "<script>
+	alert('El campo no se ha podido eliminar')
+	location.href='inventario.php';
+	</script>";
+
+ ?>
+
+
+
+
+
+
+
+	</h1></center>
+
+</body>
+</html>
